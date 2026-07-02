f6958795aa9bd95de52a032b4002a73e83feda32
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-07-02 19:06:05 +0530

    day1 stack

---
+++ Stack/Day1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout<< s.size()<<endl;
+    cout<< s.empty()<<endl;
+    cout<<"Top element: "<<s.top()<<endl;
+    s.pop();
+    cout<<"Top element after pop: "<<s.top()<<endl;
+
+}
