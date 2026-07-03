fa84a20770e789c710cdfbfac7e55766ad5fa12f
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-07-03 23:21:22 +0530

    day2 stack

---
+++ Stack/Day2_5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+//this code is to reverse the stack using another stack with time complexity of O(n) and space complexity of O(n)
+
+stack<int> reverse(stack<int> &s){
+ stack<int> temp;
+ while(s.empty()==false){
+temp.push(s.top());
+s.pop();
+}
+return temp;
+}
+int main(){
+
+    stack<int> s;
+    for (int i=1; i<=5;i++){
+        s.push(i*10);
+    }
+    cout<<endl;
+    s=reverse(s);
+
+    while(s.empty()==false){
+        cout<< s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+}
