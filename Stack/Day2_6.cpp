11258681d1b4d7a8123a443bfa3c1871c8c5d40f
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-07-03 23:58:07 +0530

    day2 stack

---
+++ Stack/Day2_6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+// with using another stack and not using recursion
+void reverse(stack<int> &s){
+  stack<int> temp;
+  while(!s.empty()){
+    temp.push(s.top());
+    s.pop();
+  }
+  s=temp;
+}
+int main(){
+    stack <int> s;
+    for(int i=1;i<=5;i++){
+        s.push(i*20);
+    }
+    reverse(s);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
---
+++ Stack/Day2_7.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Function to insert an element at the bottom of the stack
+void insertAtBottom(stack<int> &s, int x) {
+    // Base Case
+    if (s.empty()) {
+        s.push(x);
+        return;
+    }
+
+    int temp = s.top();
+    s.pop();
+
+    insertAtBottom(s, x);
+
+    s.push(temp);
+}
+
+// Function to reverse the stack
+void reverse(stack<int> &s) {
+    // Base Case
+    if (s.empty()) {
+        return;
+    }
+
+    int temp = s.top();
+    s.pop();
+
+    reverse(s);
+
+    insertAtBottom(s, temp);
+}
+
+int main() {
+    stack<int> s;
+
+    for (int i = 1; i <= 5; i++) {
+        s.push(i * 10);
+    }
+
+    reverse(s);
+
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
