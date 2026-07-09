3d3ac0ec3c90d7583876340131ba948a55f0a92d
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-07-09 17:39:27 +0530

    day1 trees

--- trees/day1.cpp
+++ trees/day1.cpp
@@ -25,8 +25,46 @@ Node * createtree(){
         root->right=createtree();
         return root;
     }
+
 }
+
+void preorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    // NLR
+    cout<< root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root){
+    if(root==NULL){
+        return;}
+    // LNR
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    }
+
+void postorder(Node* root){
+    if(root==NULL){
+        return;}
+    // LRN
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+    }
+
 int main(){
     Node* root= createtree();
     cout<<"Root node is "<<root->data<<endl;
+    cout<<"Preorder traversal: ";
+    preorder(root);
+    cout<<endl;
+    cout<<"Inorder traversal: ";
+    inorder(root);  
+    cout<<endl;
+    cout<<"Postorder traversal: ";
+    postorder(root);
 }
