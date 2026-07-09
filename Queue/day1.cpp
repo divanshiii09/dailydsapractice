4900666c3912a7b864e6ed5c8fdadee5713f92fd
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-07-09 11:42:42 +0530

    day1 queue

--- Queue/day1.cpp
+++ Queue/day1.cpp
@@ -7,7 +7,28 @@ int main(){
     q.push(10);
     q.push(20);
     q.push(30);
-    cout << q.front() << " " << q.back() << endl;
-    q.pop();
-    cout << q.front() << " " << q.back() << endl;
+    q.push(40);
+    q.push(50);
+    // cout << q.front() << " " << q.back() << endl;
+    // while(!q.empty()){
+    //     cout<< q.front()<<" ";
+    //     q.pop();
+    // }
+
+
+    deque <int> dq;
+    dq.push_front(10);
+    dq.push_front(20);
+    dq.push_back(30);
+    dq.push_back(40);
+    dq.push_front(50);
+    cout << dq.front() << " " << dq.back() << endl;
+    dq.pop_back();
+    dq.pop_front();
+    while(!dq.empty()){
+        cout<< dq.front()<<" ";
+        dq.pop_front();
+    }
+
+  
 }
